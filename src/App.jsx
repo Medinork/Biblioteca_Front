a5bdfa4d8f73f85e64a411d6758ae2f4647d0053
ANTONY RICHARD KUTINSKI MEDINO <antony.medino@alunos.sc.senac.br>
2024-09-12 19:33:26 -0300

    Feature: adiconado tela de login e sua api no api.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import LivroList from "./components/LivroList";
 import UsuarioList from "./components/UsuarioList";
 import Home from "./components/Home";
+import Login from "./components/Login"; // Importando o componente Login
 import "./App.css";
 
 function App() {
@@ -20,12 +21,16 @@ function App() {
             <li>
               <Link to="/usuarios">Gerenciar Usuários</Link>
             </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
           </ul>
         </nav>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/livros" component={LivroList} />
           <Route path="/usuarios" component={UsuarioList} />
+          <Route path="/login" component={Login} /> {/* Rota do Login */}
         </Switch>
       </div>
     </Router>
